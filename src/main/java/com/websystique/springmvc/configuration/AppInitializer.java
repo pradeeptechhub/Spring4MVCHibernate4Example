f97f0f378c5fcce06d6306618cb95068c084f651
pradeep <pradeep.techhub@gmail.com>
2017-02-19 00:28:46 +0530

    updated @Override

--- src/main/java/com/websystique/springmvc/configuration/AppInitializer.java
+++ src/main/java/com/websystique/springmvc/configuration/AppInitializer.java
@@ -10,14 +10,14 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class AppInitializer implements WebApplicationInitializer {
 
+	@Override
 	public void onStartup(ServletContext container) throws ServletException {
 
 		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
 		ctx.register(AppConfig.class);
 		ctx.setServletContext(container);
 
-		ServletRegistration.Dynamic servlet = container.addServlet(
-				"dispatcher", new DispatcherServlet(ctx));
+		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
 
 		servlet.setLoadOnStartup(1);
 		servlet.addMapping("/");
